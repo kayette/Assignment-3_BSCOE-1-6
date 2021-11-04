5c1d7f4907e8fe14599812cda96fd610da4bd1f1
Vienne Kayette Javier <viennekayetteljavier@iskolarngbayan.pup.edu.ph>
2021-11-04 13:21:58 +0800

    Create Assignment 3_APPLE-PRICE.py

---
+++ Assignment 3_APPLE-PRICE.py
@@ -0,0 +1,37 @@
+import math
+
+def getMoney():
+    myMoney=float(input("\nEnter the amount of money you have: \n= "))
+    return myMoney
+
+def getApplePrice():
+    applePrice=float(input("\nEnter your desired price for one apple: \n= "))
+    return applePrice
+
+def getAppleAmount(getMoneyF, getApplePriceF):
+    maxApple=(getMoneyF/getApplePriceF)
+    return maxApple
+
+def changeAmount(getMoneyF, getApplePriceF):
+    totalPrice=(totalApple * getApplePriceF)
+    changePrice=(getMoneyF - totalPrice)
+    return changePrice
+
+def neededAmount(getApplePriceF, getMoneyF):
+    needed=(getApplePriceF-getMoneyF)
+    return needed
+
+def finalTotal(getAppleAmountF):
+    if getAppleAmountF > 1:
+        return print(f"\nYou can buy {getAppleAmountF} apples and your change is {changeTotal:,.2f} pesos.\n")
+    elif getAppleAmountF == 1:
+        return print(f"\nYou can buy {getAppleAmountF} apples and your change is {changeTotal:,.2f} pesos.\n")
+    else:
+        return print(f"\nYou cannot buy any apples. You need {need:,.2f} more pesos to complete a purchase.\n")
+
+money=getMoney()
+apple=getApplePrice()
+totalApple=math.floor(getAppleAmount(money,apple))
+changeTotal=float(changeAmount(money,apple))
+need=float(neededAmount(apple,money))
+finalTotal(totalApple)
