63ff743873e2c55cdec3756bdf50f79e6bd107e6
Vienne Kayette Javier <viennekayetteljavier@iskolarngbayan.pup.edu.ph>
2021-11-04 13:29:16 +0800

    APPLE-PRICE_1ST COMMIT
    Grammar.

--- Assignment 3_APPLE-PRICE.py
+++ Assignment 3_APPLE-PRICE.py
@@ -21,17 +21,21 @@ def neededAmount(getApplePriceF, getMoneyF):
     needed=(getApplePriceF-getMoneyF)
     return needed
 
-def finalTotal(getAppleAmountF):
-    if getAppleAmountF > 1:
+def finalTotal(getAppleAmountF,changeTotal):
+    if (getAppleAmountF > 1) and (changeTotal > 1):
         return print(f"\nYou can buy {getAppleAmountF} apples and your change is {changeTotal:,.2f} pesos.\n")
-    elif getAppleAmountF == 1:
-        return print(f"\nYou can buy {getAppleAmountF} apples and your change is {changeTotal:,.2f} pesos.\n")
+    elif (getAppleAmountF > 1) and (changeTotal == 1):
+        return print(f"\nYou can buy {getAppleAmountF} apples and your change is {changeTotal:,.2f} peso.\n")
+    elif (getAppleAmountF == 1) and (changeTotal > 1):
+        return print(f"\nYou can buy {getAppleAmountF} apple and your change is {changeTotal:,.2f} pesos.\n")
+    elif (getAppleAmountF == 1) and (changeTotal == 1):
+        return print(f"\nYou can buy {getAppleAmountF} apple and your change is {changeTotal:,.2f} peso.\n")
     else:
-        return print(f"\nYou cannot buy any apples. You need {need:,.2f} more pesos to complete a purchase.\n")
+        return print(f"\nYou cannot buy any apples. You need {need:,.2f} more peso to complete a purchase.\n")
 
 money=getMoney()
 apple=getApplePrice()
 totalApple=math.floor(getAppleAmount(money,apple))
 changeTotal=float(changeAmount(money,apple))
 need=float(neededAmount(apple,money))
-finalTotal(totalApple)
+finalTotal(totalApple,changeTotal)
